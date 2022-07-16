dbbef96733f52aac8986a8562fe6a0a9cdabbb17
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-07-16 23:13:58 +0100

    Pause System & Pause Menu progress
     - Pausing now updates the timeScale. If pausing, it sets it to 0, otherwise it will set it to the timeScale before pausing
     - Created PauseMenu class for triggering pausing and menu

--- GMTK2022/Assets/Scripts/PauseManager.cs
+++ GMTK2022/Assets/Scripts/PauseManager.cs
@@ -6,6 +6,7 @@ public static class PauseManager
 {
     public static bool canPause = true;
     public static bool paused = false;
+    public static bool hasPausedOnce = false;
 
     // Events
     public delegate void OnPause();
@@ -14,6 +15,8 @@ public static class PauseManager
     public static event OnUnpause onUnpause;
     public delegate void OnPauseValueChanged(bool paused);
     public static event OnPauseValueChanged onPauseValueChanged;
+
+    public static float timeScaleBeforePause = 1.0f;
 
     public static void TogglePause()
     {
@@ -38,10 +41,18 @@ public static class PauseManager
 
         if (value)
         {
+            if (!hasPausedOnce)
+                hasPausedOnce = true;
+
+            timeScaleBeforePause = Time.timeScale;
+            Time.timeScale = 0.0f;
             onPause?.Invoke();
         }
         else
         {
+            if (hasPausedOnce)
+                Time.timeScale = timeScaleBeforePause;
+
             onUnpause?.Invoke();
         }
     }
