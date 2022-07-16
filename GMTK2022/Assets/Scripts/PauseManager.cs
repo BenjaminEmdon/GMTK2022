c6de723f22e374d79b3071a4c488a16b7c1859cf
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-07-16 22:41:57 +0100

    Some progress on handling pausing/unpausing

---
+++ GMTK2022/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PauseManager
+{
+    public static bool canPause = true;
+    public static bool paused = false;
+
+    // Events
+    public delegate void OnPause();
+    public static event OnPause onPause;
+    public delegate void OnUnpause();
+    public static event OnUnpause onUnpause;
+    public delegate void OnPauseValueChanged(bool paused);
+    public static event OnPauseValueChanged onPauseValueChanged;
+
+    public static void TogglePause()
+    {
+        SetPaused(!paused);
+    }
+
+    public static void Pause()
+    {
+        SetPaused(true);
+    }
+
+    public static void Unpause()
+    {
+        SetPaused(false);
+    }
+
+    public static void SetPaused(bool value)
+    {
+        paused = value;
+
+        onPauseValueChanged.Invoke(value);
+
+        if (value)
+        {
+            onPause.Invoke();
+        }
+        else
+        {
+            onUnpause.Invoke();
+        }
+    }
+}
