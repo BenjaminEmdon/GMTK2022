4105659d5cd804f74049fcea74aa1b3019a9dc0a
BenjaminEmdon <be230057@falmouth.ac.uk>
2022-07-16 12:53:52 +0100

    made player attack enum

---
+++ GMTK2022/Assets/Scripts/EPlayerAttacks.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EPlayerAttacks : MonoBehaviour
+{
+  public enum Attacks
+  {
+    Hit,
+    Miss,
+    DoubleHit,
+    QuadHit,
+    HitAll,
+    DoubleHitAll,
+    Heal
+ 
+}
+}
