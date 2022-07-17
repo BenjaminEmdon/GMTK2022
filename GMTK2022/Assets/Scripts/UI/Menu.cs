83ea5eaacd32a9bdcef985c21c95104e22529188
BenjaminEmdon <be230057@falmouth.ac.uk>
2022-07-17 15:14:53 +0100

    Created MainMenu
    should be fully functional

--- GMTK2022/Assets/Scripts/UI/Menu.cs
+++ GMTK2022/Assets/Scripts/UI/Menu.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    private const string mainMenuSceneName = "";
+   
+   
+    private const string mainMenuSceneName = "MainMenu";
+    [SerializeField] private PlayerData playerData;
+   
 
+    void Start()
+   {
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName(mainMenuSceneName))
+        {
+              playerData.nextLevel = 1;
+        }
+        
+   }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(playerData.nextLevel);
+    }
+    
+   
     public void QuitToMainMenu()
     {
         SceneManager.LoadScene(mainMenuSceneName);
